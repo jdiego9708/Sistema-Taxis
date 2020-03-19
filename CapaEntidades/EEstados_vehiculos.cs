ccbbc3652784d9d7e2c4a995f6f32c95d1ad021d
jdiego9708 <jdiego9708@users.noreply.github.com>
2020-03-19 18:44:31 -0500

    Update capaentiendades

---
+++ CapaEntidades/EEstados_vehiculos.cs
@@ -0,0 +1,89 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CapaEntidades
+{
+    public class EEstados_vehiculos
+    {
+        public EEstados_vehiculos()
+        {
+
+        }
+
+        public EEstados_vehiculos(DataRow row)
+        {
+            try
+            {
+                this.Id_estado = Convert.ToInt32(row["Id_estado"]);
+                this.Nombre_estado = Convert.ToString(row["Nombre_estado"]);
+                this.Alias_estado = Convert.ToString(row["Alias_estado"]);
+                this.Color_estado = Convert.ToString(row["Color_estado"]);
+                this.Enabled = Convert.ToString(row["Enabled"]);
+            }
+            catch (Exception ex)
+            {
+                this.OnError?.Invoke(ex.Message, null);
+            }
+        }
+
+        public EEstados_vehiculos(DataTable dt, int fila)
+        {
+            try
+            {
+                this.Id_estado = Convert.ToInt32(dt.Rows[fila]["Id_estado"]);
+                this.Nombre_estado = Convert.ToString(dt.Rows[fila]["Nombre_estado"]);
+                this.Alias_estado = Convert.ToString(dt.Rows[fila]["Alias_estado"]);
+                this.Color_estado = Convert.ToString(dt.Rows[fila]["Color_estado"]);
+                this.Enabled = Convert.ToString(dt.Rows[fila]["Enabled"]);
+            }
+            catch (Exception ex)
+            {
+                this.OnError?.Invoke(ex.Message, null);
+            }
+        }
+
+        public static DataTable BuscarEstados(string tipo_busqueda, string texto_busqueda, out string rpta)
+        {
+            return DEstados_vehiculos.BuscarEstados(tipo_busqueda, texto_busqueda, out rpta);
+        }
+
+        public static string InsertarEstado(EEstados_vehiculos estado, out int id_estado)
+        {
+            List<string> vs = new List<string>
+            {
+               estado.Nombre_estado,estado.Alias_estado,
+               estado.Color_estado, estado.Enabled
+            };
+            return DEstados_vehiculos.InsertarEstadoVehiculo(out id_estado, vs);
+        }
+
+        public static string EditarEstado(EEstados_vehiculos estado, int id_estado)
+        {
+            List<string> vs = new List<string>
+            {
+               estado.Nombre_estado,estado.Alias_estado,
+               estado.Color_estado, estado.Enabled
+            };
+            return DEstados_vehiculos.EditarEstadoVehiculo(id_estado, vs);
+        }
+
+
+        private int _id_estado;
+        private string _nombre_estado;
+        private string _alias_estado;
+        private string _color_estado;
+        private string _enabled;
+
+        public int Id_estado { get => _id_estado; set => _id_estado = value; }
+        public string Nombre_estado { get => _nombre_estado; set => _nombre_estado = value; }
+        public string Alias_estado { get => _alias_estado; set => _alias_estado = value; }
+        public string Color_estado { get => _color_estado; set => _color_estado = value; }
+        public string Enabled { get => _enabled; set => _enabled = value; }
+
+        public event EventHandler OnError;
+
+    }
+}
---
+++ CapaPresentacion/Formularios/FormsEstadosVehiculos/FrmNuevoEstado.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CapaPresentacion.Formularios.FormsEstadosVehiculos
+{
+    partial class FrmNuevoEstado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmNuevoEstado));
+            this.gbCodigo = new System.Windows.Forms.GroupBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gbCodigo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbCodigo
+            // 
+            this.gbCodigo.Controls.Add(this.txtCodigo);
+            this.gbCodigo.Location = new System.Drawing.Point(12, 12);
+            this.gbCodigo.Name = "gbCodigo";
+            this.gbCodigo.Size = new System.Drawing.Size(429, 56);
+            this.gbCodigo.TabIndex = 2;
+            this.gbCodigo.TabStop = false;
+            this.gbCodigo.Text = "Nombre del estado";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCodigo.BackColor = System.Drawing.Color.White;
+            this.txtCodigo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCodigo.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtCodigo.Location = new System.Drawing.Point(7, 24);
+            this.txtCodigo.MaxLength = 13;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(416, 22);
+            this.txtCodigo.TabIndex = 1;
+            this.txtCodigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(234, 216);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Color";
+            // 
+            // FrmNuevoEstado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(453, 410);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbCodigo);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmNuevoEstado";
+            this.Text = "Nuevo estado";
+            this.gbCodigo.ResumeLayout(false);
+            this.gbCodigo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.GroupBox gbCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        public System.Windows.Forms.GroupBox groupBox1;
+    }
+}
